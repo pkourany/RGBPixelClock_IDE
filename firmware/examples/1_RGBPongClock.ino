71d3717fd7c4584ed41c6aca7ef451181f2d58e0
Paul Kourany <pkourany@users.noreply.github.com>
2016-05-20 17:42:50 -0400

    Update 1_RGBPongClock.ino

--- firmware/examples/1_RGBPongClock.ino
+++ firmware/examples/1_RGBPongClock.ino
@@ -1,4 +1,5 @@
-/*  RGB Pong Clock - Andrew Holmes @pongclock
+/*  RGB Pong Clock
+**  Adapted by Paul Kourany, @peekay123 from Andrew Holmes @pongclock
 **  Inspired by, and shamelessly derived from 
 **      Nick's LED Projects
 **  https://123led.wordpress.com/about/
@@ -7,18 +8,15 @@
 **  https://vine.co/v/hwML6OJrBPw
 **  https://vine.co/v/hgKWh1KzEU0
 **  https://vine.co/v/hgKz5V0jrFn
-**  I run this on a Mega 2560, your milage on other chips may vary,
-**  Can definately free up some memory if the bitmaps are shrunk down to size.
-**  Uses an Adafruit 16x32 RGB matrix availble from here:
-**  http://www.phenoptix.com/collections/leds/products/16x32-rgb-led-matrix-panel-by-adafruit
-**  This microphone:
-**  http://www.phenoptix.com/collections/adafruit/products/electret-microphone-amplifier-max4466-with-adjustable-gain-by-adafruit-1063
-**  a DS1307 RTC chip (not sure where I got that from - was a spare)
-**  and an Ethernet Shield
-**  http://hobbycomponents.com/index.php/dvbd/dvbd-ardu/ardu-shields/2012-ethernet-w5100-network-shield-for-arduino-uno-mega-2560-1280-328.html
-** 
+**
+**  Please refer to  https://github.com/pkourany/RGBPixelClock_IDE for details
 */
 
+/* The following libraries MUST be included:
+** Adafruit_mfGFX
+** SparkIntervalTimer
+** RGBmatrixPanel
+*/
 
 #include "RGBPixelClock/RGBPixelClock.h"   // Core graphics library
 #include "Adafruit_mfGFX/Adafruit_mfGFX.h"   // Core graphics library
